28ffd384bf0e607ea5598f8764903ec544c07328
huangguizhao <2678383176@qq.com>
2019-10-31 11:38:40 +0800

    实现分页的抽取

--- v17-web/v17-center/src/main/java/com/hgz/v17center/V17CenterApplication.java
+++ v17-web/v17-center/src/main/java/com/hgz/v17center/V17CenterApplication.java
@@ -3,8 +3,9 @@ package com.hgz.v17center;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDubbo
 public class V17CenterApplication {
 
